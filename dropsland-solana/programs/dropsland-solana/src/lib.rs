d0cab61ebde427d30210a33807d0a0999612fa36
jovemjeune <thinklikeflasher@gmail.com>
2025-10-23 18:05:43 -0300

    soulbound token minter module added

--- dropsland-solana/programs/dropsland-solana/src/lib.rs
+++ dropsland-solana/programs/dropsland-solana/src/lib.rs
@@ -1,5 +1,11 @@
 use anchor_lang::prelude::*;
 
+// Import the soulbound token minter module
+pub mod soulbound_token_minter;
+
+// Re-export the soulbound token minter program
+pub use soulbound_token_minter::*;
+
 declare_id!("2EpreJPoJC6wEHk3hShxffGyPbmEaNLyDMKQmbSsTWXH");
 
 #[program]
